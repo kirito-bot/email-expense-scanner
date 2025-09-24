b2ef5a3144961d2dff883af062f850fdfca8bd51
biplo <biplop2168@gmail.com>
2025-09-24 08:08:22 -0400

    initial commit

---
+++ src/main/java/com/example/email_expense_scanner/EmailExpenseScannerApplication.java
@@ -0,0 +1,13 @@
+package com.example.email_expense_scanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailExpenseScannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailExpenseScannerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/email_expense_scanner/EmailExpenseScannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.email_expense_scanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailExpenseScannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
