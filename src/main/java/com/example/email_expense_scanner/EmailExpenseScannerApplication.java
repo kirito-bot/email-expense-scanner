0bbb97c56dc4f436c3774c8f2db0cc2d21b8db78
biplo <biplop2168@gmail.com>
2025-09-24 08:40:09 -0400

    added logs

--- src/main/java/com/example/email_expense_scanner/EmailExpenseScannerApplication.java
+++ src/main/java/com/example/email_expense_scanner/EmailExpenseScannerApplication.java
@@ -3,11 +3,15 @@ package com.example.email_expense_scanner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.logging.Logger;
+
 @SpringBootApplication
 public class EmailExpenseScannerApplication {
-
+	private static final Logger logger = Logger.getLogger(EmailExpenseScannerApplication.class.getName());
 	public static void main(String[] args) {
+
 		SpringApplication.run(EmailExpenseScannerApplication.class, args);
+		logger.info("Email Expense Scanner Application started successfully.");
 	}
 
 }
